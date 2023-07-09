baea57a61fc1b651c1f1cd174afd31d4e65d60ca
oSumAtrIX <johan.melkonyan1@web.de>
2023-07-09 03:47:52 +0200

    feat: init

---
+++ src/commands.rs
@@ -0,0 +1,91 @@
+use poise::serenity_prelude::{CacheHttp, ChannelId, GuildId};
+
+use crate::{Context, Error, CLYDE_ID};
+
+/// Show this help menu
+#[poise::command(prefix_command, track_edits, slash_command)]
+pub async fn help(
+    ctx: Context<'_>,
+    #[description = "Specific command to show help about"]
+    #[autocomplete = "poise::builtins::autocomplete_command"]
+    command: Option<String>,
+) -> Result<(), Error> {
+    poise::builtins::help(
+        ctx,
+        command.as_deref(),
+        poise::builtins::HelpConfiguration {
+            extra_text_at_bottom: "This is an example bot made to showcase features of my custom Discord bot framework",
+            ..Default::default()
+        },
+    )
+    .await?;
+    Ok(())
+}
+
+/// Register slash commands
+#[poise::command(prefix_command, owners_only)]
+pub async fn register(ctx: Context<'_>) -> Result<(), Error> {
+    poise::builtins::register_application_commands_buttons(ctx).await?;
+    Ok(())
+}
+
+/// Pair Clyde from another Discord server with this server.
+#[poise::command(slash_command, owners_only)]
+pub async fn pair(
+    ctx: Context<'_>,
+    #[description = "The guild ID of the other server"] guild_id: String,
+    #[description = "The channel ID of the other server"] channel_id: String,
+) -> Result<(), Error> {
+    let guild_id = GuildId(guild_id.parse()?);
+    let channel_id = ChannelId(channel_id.parse()?);
+    let to_channel_id = ctx.channel_id();
+
+    let cache = ctx.cache().unwrap();
+
+    let Some(guild) = cache.guild(guild_id) else {
+		return Err("Guild not found".into());
+	 };
+
+    if let None = cache.channel(channel_id.0) {
+        return Err("Channel not found".into());
+    };
+
+    {
+        let clone = ctx.data().pair_config.clone();
+        let mut config = clone.lock().await;
+        config.guild_id = guild_id;
+        config.from_channel_id = channel_id;
+        config.to_channel_id = to_channel_id;
+    }
+
+    ctx.say(format!(
+        "Proxying <@{}> in channel <#{}> from guild {} to channel {}.",
+        CLYDE_ID, channel_id, guild.name, to_channel_id.0
+    ))
+    .await?;
+    Ok(())
+}
+
+/// Send a message to the paired server.
+#[poise::command(slash_command, prefix_command, owners_only)]
+pub async fn message(
+    ctx: Context<'_>,
+    #[description = "The message to send"] message: String,
+) -> Result<(), Error> {
+    let config = ctx.data().pair_config.lock().await;
+
+    config
+        .from_channel_id
+        .say(
+            ctx.http(),
+            format!(
+                "<@{}> Hello, my name is {}. {}",
+                CLYDE_ID,
+                ctx.author().name,
+                message
+            ),
+        )
+        .await?;
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,150 @@
+#![warn(clippy::str_to_string)]
+
+mod commands;
+
+use poise::{
+    futures_util::lock::Mutex,
+    serenity_prelude::{self as serenity, ChannelId, GuildId, UserId},
+};
+use std::{env::var, sync::Arc};
+use tokio::sync::RwLock;
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+#[derive(Default)]
+pub struct Data {
+    pair_config: Arc<Mutex<PairConfiguration>>,
+}
+
+#[derive(Default)]
+pub struct PairConfiguration {
+    guild_id: GuildId,
+    from_channel_id: ChannelId,
+    to_channel_id: ChannelId,
+}
+
+#[derive(Default)]
+struct Handler {
+    options: poise::FrameworkOptions<Data, Error>,
+    data: Data,
+    bot_id: RwLock<Option<UserId>>,
+    shard_manager:
+        std::sync::Mutex<Option<std::sync::Arc<tokio::sync::Mutex<serenity::ShardManager>>>>,
+}
+
+const CLYDE_ID: u64 = 1081004946872352958;
+
+// Custom handler to dispatch poise events.
+impl Handler {
+    pub fn new(options: poise::FrameworkOptions<Data, Error>) -> Self {
+        Self {
+            options,
+            ..Default::default()
+        }
+    }
+
+    async fn dispatch_poise_event(&self, ctx: &serenity::Context, event: &poise::Event<'_>) {
+        let framework_data = poise::FrameworkContext {
+            bot_id: self.bot_id.read().await.unwrap(),
+            options: &self.options,
+            user_data: &self.data,
+            shard_manager: &(*self.shard_manager.lock().unwrap()).clone().unwrap(),
+        };
+
+        poise::dispatch_event(framework_data, ctx, event).await;
+    }
+}
+
+#[serenity::async_trait]
+impl serenity::EventHandler for Handler {
+    async fn message(&self, ctx: serenity::Context, new_message: serenity::Message) {
+        if new_message.author.bot && new_message.author.id == CLYDE_ID {
+            let data = self.data.pair_config.lock().await;
+
+            if data.to_channel_id == new_message.channel_id {
+                let _ = data
+                    .to_channel_id
+                    .say(&ctx.http, &new_message.content)
+                    .await;
+            }
+        }
+
+        self.dispatch_poise_event(&ctx, &poise::Event::Message { new_message })
+            .await;
+    }
+
+    async fn interaction_create(&self, ctx: serenity::Context, interaction: serenity::Interaction) {
+        self.dispatch_poise_event(&ctx, &poise::Event::InteractionCreate { interaction })
+            .await;
+    }
+
+    async fn ready(&self, _ctx: serenity::Context, ready: serenity::Ready) {
+        *self.bot_id.write().await = Some(ready.user.id);
+    }
+}
+
+async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
+    match error {
+        poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
+        poise::FrameworkError::Command { error, ctx } => {
+            println!("Error in command `{}`: {:?}", ctx.command().name, error,);
+        }
+        error => {
+            if let Err(e) = poise::builtins::on_error(error).await {
+                println!("Error while handling error: {}", e)
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    env_logger::init();
+    dotenv::dotenv().expect("Failed to read .env file");
+
+    let options = poise::FrameworkOptions {
+        commands: vec![
+            commands::help(),
+            commands::register(),
+            commands::pair(),
+            commands::message(),
+        ],
+        prefix_options: poise::PrefixFrameworkOptions {
+            mention_as_prefix: true,
+            ..Default::default()
+        },
+        on_error: |error| Box::pin(on_error(error)),
+        pre_command: |ctx| {
+            Box::pin(async move {
+                println!("Executing command {}", ctx.command().qualified_name);
+            })
+        },
+        owners: vec![UserId(
+            var("OWNER_ID")
+                .expect("Missing `OWNER_ID` environment variable")
+                .parse::<u64>()
+                .expect("Failed to parse `OWNER_ID` environment variable"),
+        )]
+        .into_iter()
+        .collect(),
+        skip_checks_for_owners: false,
+        ..Default::default()
+    };
+
+    let handler = std::sync::Arc::new(Handler::new(options));
+
+    let mut client = serenity::Client::builder(
+        var("DISCORD_TOKEN").expect("Missing `DISCORD_TOKEN` environment variable"),
+        serenity::GatewayIntents::non_privileged()
+            | serenity::GatewayIntents::MESSAGE_CONTENT
+            | serenity::GatewayIntents::GUILD_MESSAGES,
+    )
+    .event_handler_arc(handler.clone())
+    .await?;
+
+    *handler.shard_manager.lock().unwrap() = Some(client.shard_manager.clone());
+    client.start().await?;
+
+    Ok(())
+}
